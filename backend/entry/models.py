b4dffafef7103f89de79aed60e1b23b40d20936c
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-31 19:32:59 -0600

    created model for entry app

--- backend/entry/models.py
+++ backend/entry/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Entry(models.Model):
+    habit = models.CharField(max_length=100)
+    date = models.DateField()
+    unit = models.IntegerField()
