d830e5806a9aa35ee28e974793cd2705a27ec693
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-11-02 15:18:23 -0600

    registered apps in admin and project settings + made migrations

--- backend/entry/models.py
+++ backend/entry/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from habit.models import Habit
 
 class Entry(models.Model):
-    habit = models.ForeignKay(Habit, on_delete=models.CASCADE)
+    habit = models.ForeignKey(Habit, on_delete=models.CASCADE)
     date = models.DateField()
     amount = models.IntegerField()
