f637ef68d533252411955494f2164945df4629a8
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-11-08 18:57:58 -0700

    created post functionality for habit app

--- backend/habit/views.py
+++ backend/habit/views.py
@@ -5,9 +5,14 @@ from rest_framework.response import Response
 from .models import Habit
 from .serializers import HabitSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def habit_list(request):
     if request.method == 'GET':
         habit = Habit.objects.all()
         serializer = HabitSerializer(habit, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = HabitSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
