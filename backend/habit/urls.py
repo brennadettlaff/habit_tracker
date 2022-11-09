55f7fce2472fac041bf85cc81846f4775e32bc48
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-11-08 19:10:09 -0700

    created get habit by id functionality and url path in habit app

--- backend/habit/urls.py
+++ backend/habit/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.habit_list),
+    path('<int:pk>/',views.habit_detail)
 ]
--- backend/habit/views.py
+++ backend/habit/views.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Habit
 from .serializers import HabitSerializer
@@ -16,3 +17,10 @@ def habit_list(request):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+@api_view(['GET'])
+def habit_detail(request, pk):
+    habit = get_object_or_404(Habit, pk=pk)
+    if request.method == 'GET':
+        serializer = HabitSerializer(habit)
+        return Response(serializer.data)
