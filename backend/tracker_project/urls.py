d5a4ad9f6cd54bc9e3da53582388fb268b3cc970
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-11-20 19:43:34 -0700

    created url path for entry app in project settings

--- backend/tracker_project/urls.py
+++ backend/tracker_project/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/habit/', include('habit.urls')),
+    path('api/entry/', include('entry.urls')),
 ]
