011d1bc1d4d2a769ab7e59c00702861630bf1b6b
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-11-22 16:07:54 -0700

    created add entry functionality

--- backend/entry/views.py
+++ backend/entry/views.py
@@ -6,12 +6,17 @@ from django.shortcuts import get_object_or_404
 from .models import Entry
 from .serializers import EntrySerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def all_entries(request):
     if request.method == 'GET':
         entry = Entry.objects.all()
         serializer = EntrySerializer(entry, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = EntrySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 @api_view(['GET'])
 def entry_list(request, habit_id):
