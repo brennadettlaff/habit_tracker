e29ccc5e868108203f6a92b5e30fb221140bd9f2
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-06 20:25:59 -0700

    installed cors headers

--- backend/tracker_project/settings.py
+++ backend/tracker_project/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'habit',
     'entry',
 ]
@@ -47,7 +48,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'corsheaders.middleware.CorsMiddleware',
+    ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'tracker_project.urls'
 
