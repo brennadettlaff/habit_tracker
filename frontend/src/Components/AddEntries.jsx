c348625de8c02d809adc0c7a048c2fa1903a53d8
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-10 16:42:17 -0700

    created form for adding new entry

--- frontend/src/Components/AddEntries.jsx
+++ frontend/src/Components/AddEntries.jsx
@@ -1,7 +1,19 @@
+import React, { useState } from 'react';
 const AddEntries = () => {
+    const [habit, setHabit] = useState(0);
+    const [date, setDate] = useState('');
+    const [amount, setAmount] = useState(0);
+
     return ( 
         <div>
-            
+            <form>
+                <label>Habit</label>
+                <input type='number' value={habit} />
+                <label>Date</label>
+                <input type='date' value={date} />
+                <label>Amount</label>
+                <input type='number' value={amount} />
+            </form>
         </div>
      );
 }
