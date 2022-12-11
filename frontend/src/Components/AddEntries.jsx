1293becc2d33b6b00d245105d12768d1362970ce
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-10 17:33:54 -0700

    created handle submit functionality

--- frontend/src/Components/AddEntries.jsx
+++ frontend/src/Components/AddEntries.jsx
@@ -4,15 +4,24 @@ const AddEntries = () => {
     const [date, setDate] = useState('');
     const [amount, setAmount] = useState(0);
 
+    function handleSubmit(event) {
+        event.preventDefault();
+        let newEntry = {
+            habit: habit,
+            date: date,
+            amount: amount,
+        }
+        console.log(newEntry)
+    }
     return ( 
         <div>
             <form>
                 <label>Habit</label>
-                <input type='number' value={habit} />
+                <input type='number' value={habit} onChange={(event) => setHabit(event.target.value)}/>
                 <label>Date</label>
-                <input type='date' value={date} />
+                <input type='date' value={date} onChange={(event) => setDate(event.target.value)}/>
                 <label>Amount</label>
-                <input type='number' value={amount} />
+                <input type='number' value={amount} onChange={(event) => setAmount(event.target.value)}/>
             </form>
         </div>
      );
