090f5d114132356d375c6cd03ee5f73228d98976
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-10 17:41:34 -0700

    added button to submit changes

--- frontend/src/Components/AddEntries.jsx
+++ frontend/src/Components/AddEntries.jsx
@@ -15,13 +15,14 @@ const AddEntries = () => {
     }
     return ( 
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Habit</label>
                 <input type='number' value={habit} onChange={(event) => setHabit(event.target.value)}/>
                 <label>Date</label>
                 <input type='date' value={date} onChange={(event) => setDate(event.target.value)}/>
                 <label>Amount</label>
                 <input type='number' value={amount} onChange={(event) => setAmount(event.target.value)}/>
+                <button type='submit'>Add</button>
             </form>
         </div>
      );
